cc8a2369baf259f86d8862f95de47d13bedddd56
Oliwier1337 <oliwier.bujwid@infotech.edu.pl>
2021-11-22 08:06:19 +0100

    create repo

---
+++ kolko_krzyzyk/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace kolko_krzyzyk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
