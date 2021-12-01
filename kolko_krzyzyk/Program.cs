9c40b270e366e923f1467dcd37d29e229c14737f
Oliwier1337 <94827315+Oliwier1337@users.noreply.github.com>
2021-12-01 10:34:23 +0100

    3 commit
    plansza do gry

--- kolko_krzyzyk/Program.cs
+++ kolko_krzyzyk/Program.cs
@@ -89,3 +89,23 @@ namespace Kolko_i_Krzyzyk
             Console.ReadLine();
         }
         
+        private static void Board()
+        {
+            Console.WriteLine("     |     |    ");
+
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[1], arr[2], arr[3]);
+
+            Console.WriteLine("____|_____|____ ");
+
+            Console.WriteLine("     |     |      ");
+
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[4], arr[5], arr[6]);
+
+            Console.WriteLine("____|_____|____ ");
+
+            Console.WriteLine("     |     |      ");
+
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[7], arr[8], arr[9]);
+
+            Console.WriteLine("     |     |      ");
+        }
