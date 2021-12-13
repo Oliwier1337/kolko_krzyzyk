dab043cb33de57ececf628f237fdece9eafa007e
adamekk11 <94827223+adamekk11@users.noreply.github.com>
2021-12-13 08:29:25 +0100

    dokończenie sprawdzania wygranej, dokończenie projektu

--- kolko_krzyzyk/Program.cs
+++ kolko_krzyzyk/Program.cs
@@ -88,8 +88,8 @@ namespace Kolko_i_Krzyzyk
 
             Console.ReadLine();
         }
-        
-        /* Plansza do gry */
+
+
         private static void Board()
         {
             Console.WriteLine("     |     |    ");
@@ -110,12 +110,75 @@ namespace Kolko_i_Krzyzyk
 
             Console.WriteLine("     |     |      ");
         }
-        
-        /* Sprawdzanie czy któryś z graczy wygrał */
+  
         private static int CheckWin()
         {
             if (arr[1] == arr[2] && arr[2] == arr[3])
-                {
-                    return 1;
-                }
+            {
+                return 1;
+            }
+
+            else if (arr[4] == arr[5] && arr[5] == arr[6])
+            {
+                return 1;
+            }
+
+            else if (arr[6] == arr[7] && arr[7] == arr[8])
+            {
+                return 1;
+            }
+
+
+
+
+
+            else if (arr[1] == arr[4] && arr[4] == arr[7])
+            {
+                return 1;
+            }
+
+            else if (arr[2] == arr[5] && arr[5] == arr[8])
+            {
+                return 1;
+            }
+
+            else if (arr[3] == arr[6] && arr[6] == arr[9])
+
+            {
+                return 1;
+            }
+
+
+
+
+
+            else if (arr[1] == arr[5] && arr[5] == arr[9])
+            {
+                return 1;
+            }
+
+            else if (arr[3] == arr[5] && arr[5] == arr[7])
+            {
+                return 1;
+            }
+
+
+
+
+            else if (arr[1] != '1' && arr[2] != '2' && arr[3] != '3' && arr[4] != '4' && arr[5] != '5' && arr[6] != '6' && arr[7] != '7' && arr[8] != '8' && arr[9] != '9')
+            {
+                return -1;
+            }
+
+
+
+            else
+            {
+                return 0;
+            }
+
         }
+
+    }
+
+}
